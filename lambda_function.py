b2d042d419964b77726f2c77df85dd71d4fec524
EC2 Default User <ec2-user@ip-10-0-0-114.us-east-2.compute.internal>
2020-01-23 20:38:11 +0900

    STAGE環境完成

--- lambda_function.py
+++ lambda_function.py
@@ -2,7 +2,7 @@ import json
 import codecs
   
 def lambda_handler(event, context):
-    print("Hello kinkoman", file=codecs.open('hello.txt', 'w', 'utf-8'))
+    #print("Hello kinkoman", file=codecs.open('hello.txt', 'w', 'utf-8'))
     return event
 
 
